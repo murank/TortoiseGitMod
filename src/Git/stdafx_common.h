dc1e2f25acf2e63d6c3fd6ff5963466da4ba77f2
murank <murank42@gmail.com>
2011-10-08 16:10:07 +0900

    Organize stdafx.h in Git project. #3

--- src/Git/stdafx_common.h
+++ src/Git/stdafx_common.h
@@ -20,5 +20,22 @@
 #ifndef STDAFX_COMMON_H
 #define STDAFX_COMMON_H
 
+#include "..\targetver.h"
+
+#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headersicit
+#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be expl
+#include <afxwin.h>         // MFC core and standard components
+#include <afxstr.h>
+
+#include <atlbase.h>
+
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <list>
+#include <algorithm>
 
 #endif
