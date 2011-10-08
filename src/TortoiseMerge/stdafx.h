ed7f5343b9a947229aeb4d1b8534437e9904b4da
murank <murank42@gmail.com>
2011-10-08 23:00:40 +0900

    Organize stdafx.h of TortoiseMerge project. #3

--- src/TortoiseMerge/stdafx.h
+++ src/TortoiseMerge/stdafx.h
@@ -4,58 +4,24 @@
 
 #pragma once
 
-#ifndef VC_EXTRALEAN
-//#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
-#endif
+#include "stdafx_common.h"
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-
-#include "../targetver.h"
-
-#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some CString constructors will be explicit
-
-// turns off MFC's hiding of some common and often safely ignored warning messages
-#define _AFX_ALL_WARNINGS
-
-#include <afxwin.h>         // MFC core and standard components
-#include <afxext.h>         // MFC extensions
-#include <afxdisp.h>        // MFC Automation classes
-#include <afxcoll.h>		// MFC Collection templates and classes
-#include <shlwapi.h>		// Shell API
-
-#include <afxstr.h>
-
-#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
 #ifndef _AFX_NO_AFXCMN_SUPPORT
 #include <afxcmn.h>			// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
-#include <WinSock2.h>
-#include <Ws2tcpip.h>
-#include <Wspiapi.h>
-
-#include <afxdlgs.h>
+#include <afxwinappex.h>
 #include <afxctl.h>
-#include <afxext.h>         // MFC extensions
-#include <afxcontrolbars.h>     // MFC support for ribbons and control bars
-
-#ifndef LVS_EX_DOUBLEBUFFER
-#define LVS_EX_DOUBLEBUFFER     0x00010000
-#endif
-
-#pragma warning(push)
-#pragma warning(disable: 4201)	// nonstandard extension used : nameless struct/union (in MMSystem.h)
-#include <vfw.h>
-#pragma warning(pop)
-
-#pragma warning(push)
-#pragma warning(disable: 4702)	// Unreachable code warnings in xtree
-#include <string>
-#include <map>
-#include <vector>
-#pragma warning(pop)
-
-#define USE_GDI_GRADIENT
+#include <afxpanedialog.h>
+#include <afxframewndex.h>
+#include <afxstatusbar.h>
+#include <afxvisualmanagerwindows.h>
+#include <afxcolorbutton.h>
+#include <afxfontcombobox.h>
+#include <afxvisualmanageroffice2003.h>
+#include <afxvisualmanagervs2005.h>
+#include <afxvisualmanageroffice2007.h>
+#include <afxtoolbarfontcombobox.h>
 
 #define XMESSAGEBOX_APPREGPATH "Software\\TortoiseMerge\\"
 
@@ -64,3 +30,9 @@
 #else
 #	define APP_X64_STRING ""
 #endif
+
+#ifndef LVS_EX_DOUBLEBUFFER
+#define LVS_EX_DOUBLEBUFFER     0x00010000
+#endif
+
+#define USE_GDI_GRADIENT
