997fd3efdc640003cfa08dda957385351374060d
murank <murank42@gmail.com>
2011-10-29 12:08:50 +0900

    Suppress a warning of unsafe STL functions. #5

--- src/Git/stdafx_common.h
+++ src/Git/stdafx_common.h
@@ -23,6 +23,7 @@
 #include "..\targetver.h"
 
 #define _CRT_SECURE_NO_WARNINGS
+#define _SCL_SECURE_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headersicit
 #define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
 
