906eed4578e09b9dff7052659282bd9ad3ccc1bb
murank <murank42@gmail.com>
2011-11-26 20:36:16 +0900

    Create a helper macro: STATIC_INIT. #9

---
+++ src/Git/StaticInit.h
@@ -0,0 +1,34 @@
+// TortoiseGit - a Windows shell extension for easy version control
+
+// External Cache Copyright (C) 2005 - 2006 - Will Dean, Stefan Kueng
+// Copyright (C) 2008-2011 - TortoiseGit
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#ifndef STATIC_INIT_H
+#define STATIC_INIT_H
+
+#define STATIC_INIT(expr) STATIC_INIT_IMPL(expr, __LINE__)
+#define STATIC_INIT_IMPL(expr, line) STATIC_INIT_IMPL2(expr, line)
+
+#define STATIC_INIT_IMPL2(expr, line) \
+	static struct StaticInitializer_##line {	\
+		StaticInitializer_##line##() {			\
+			expr;								\
+		}										\
+	} instance_##line
+
+#endif
