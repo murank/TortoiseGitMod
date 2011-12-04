53c808602a3ead9ec0284e546dccc138994bfaf6
murank <murank42@gmail.com>
2011-12-04 21:28:35 +0900

    Add test for ConcurrentQueue. #10

--- testing/GitTest/ConcurrentQueueTest.cpp
+++ testing/GitTest/ConcurrentQueueTest.cpp
@@ -20,3 +20,94 @@
 #include "stdafx.h"
 #include <gmock/gmock.h>
 #include "TestHelper.h"
+
+#include "ConcurrentQueue.h"
+
+using namespace ::testing;
+
+namespace {
+
+class MockConcurrentQueue : public ConcurrentQueue<int> {
+public:
+
+	MOCK_METHOD1(Wait, bool(AutoLocker& lock));
+	MOCK_METHOD0(Notify, void());
+
+	void PushVal() {
+		Push(99);
+	}
+
+};
+
+} // namespace
+
+TEST(ConcurrentQueue, PopAfterCancelation)
+{
+	MockConcurrentQueue mcq;
+
+	EXPECT_CALL(mcq, Wait(_))
+		.Times(0);
+	EXPECT_CALL(mcq, Notify())
+		.Times(1);
+
+	mcq.CancelAsync();
+
+	EXPECT_THROW(mcq.Pop(), pop_interrupted);
+}
+
+TEST(ConcurrentQueue, CanceleWhileWaiting)
+{
+	MockConcurrentQueue mcq;
+
+	EXPECT_CALL(mcq, Notify())
+		.Times(1);
+	EXPECT_CALL(mcq, Wait(_))
+		.Times(1)
+		.WillOnce(DoAll(IgnoreResult(InvokeWithoutArgs(&mcq, &MockConcurrentQueue::CancelAsync)), Return(true)));
+
+	EXPECT_THROW(mcq.Pop(), pop_interrupted);
+}
+
+TEST(ConcurrentQueue, Push)
+{
+	MockConcurrentQueue mcq;
+
+	EXPECT_CALL(mcq, Notify())
+		.Times(1);
+
+	int x = 99;
+
+	mcq.Push(x);
+}
+
+TEST(ConcurrentQueue, PopAfterPushing)
+{
+	MockConcurrentQueue mcq;
+
+	EXPECT_CALL(mcq, Notify())
+		.Times(2);
+	EXPECT_CALL(mcq, Wait(_))
+		.Times(0);
+
+	int x1 = 100;
+	int x2 = 200;
+
+	mcq.Push(x1);
+	mcq.Push(x2);
+
+	EXPECT_EQ(x1, mcq.Pop());
+	EXPECT_EQ(x2, mcq.Pop());
+}
+
+TEST(ConcurrentQueue, PushWhileWaiting)
+{
+	MockConcurrentQueue mcq;
+
+	EXPECT_CALL(mcq, Notify())
+		.Times(1);
+	EXPECT_CALL(mcq, Wait(_))
+		.Times(1)
+		.WillOnce(DoAll(InvokeWithoutArgs(&mcq, &MockConcurrentQueue::PushVal), Return(true)));
+
+	EXPECT_EQ(99, mcq.Pop());
+}
