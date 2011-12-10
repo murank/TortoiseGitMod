2b19bd470d1cbf6917019e49edd572d5eee55728
murank <murank42@gmail.com>
2011-12-10 13:22:36 +0900

    Add test for CountLatch. #10

--- testing/GitTest/CountLatchTest.cpp
+++ testing/GitTest/CountLatchTest.cpp
@@ -20,3 +20,193 @@
 #include "stdafx.h"
 #include <gmock/gmock.h>
 #include "TestHelper.h"
+
+#include "CountLatch.h"
+
+using namespace ::testing;
+
+namespace {
+
+class MockCountLatch : public CountLatch {
+public:
+	MOCK_METHOD1(Wait, bool(AutoLocker& lock));
+	MOCK_METHOD0(Notify, void());
+
+	using CountLatch::SetCount;
+};
+
+} // namespace
+
+TEST(CountLatch, CountUp)
+{
+	MockCountLatch mcl;
+
+	EXPECT_CALL(mcl, Wait(_))
+		.Times(0);
+	EXPECT_CALL(mcl, Notify())
+		.Times(0);
+
+	EXPECT_EQ(0, mcl.GetCount());
+
+	EXPECT_TRUE(mcl.CountUp());
+
+	EXPECT_EQ(1, mcl.GetCount());
+}
+
+TEST(CountLatch, CountUpUntilSaturation)
+{
+	MockCountLatch mcl;
+
+	EXPECT_CALL(mcl, Wait(_))
+		.Times(0);
+	EXPECT_CALL(mcl, Notify())
+		.Times(0);
+
+	EXPECT_EQ(0, mcl.GetCount());
+
+	typedef CountLatch::counter_type c_type;
+	const c_type max_value = std::numeric_limits<CountLatch::counter_type>::max();
+
+	mcl.SetCount(max_value-1);
+
+	EXPECT_EQ(max_value-1, mcl.GetCount());
+
+	EXPECT_TRUE(mcl.CountUp());
+	EXPECT_EQ(max_value, mcl.GetCount());
+
+	EXPECT_FALSE(mcl.CountUp());
+	EXPECT_EQ(max_value, mcl.GetCount());
+}
+
+TEST(CountLatch, CountDown)
+{
+	MockCountLatch mcl;
+
+	EXPECT_CALL(mcl, Wait(_))
+		.Times(0);
+	EXPECT_CALL(mcl, Notify())
+		.Times(0);
+
+	EXPECT_EQ(0, mcl.GetCount());
+
+	mcl.CountDown();
+
+	EXPECT_EQ(-1, mcl.GetCount());
+}
+
+TEST(CountLatch, CountDownUntilSaturation)
+{
+	MockCountLatch mcl;
+
+	EXPECT_CALL(mcl, Wait(_))
+		.Times(0);
+	EXPECT_CALL(mcl, Notify())
+		.Times(0);
+
+	EXPECT_EQ(0, mcl.GetCount());
+
+	typedef CountLatch::counter_type c_type;
+	const c_type min_value = std::numeric_limits<CountLatch::counter_type>::min();
+
+	mcl.SetCount(min_value+1);
+
+	EXPECT_EQ(min_value+1, mcl.GetCount());
+
+	EXPECT_TRUE(mcl.CountDown());
+	EXPECT_EQ(min_value, mcl.GetCount());
+
+	EXPECT_FALSE(mcl.CountDown());
+	EXPECT_EQ(min_value, mcl.GetCount());
+}
+
+TEST(CountLatch, NotifyWhenCountDown)
+{
+	MockCountLatch mcl;
+
+	EXPECT_CALL(mcl, Wait(_))
+		.Times(0);
+	EXPECT_CALL(mcl, Notify())
+		.Times(0);
+
+	mcl.CountUp();
+
+	EXPECT_CALL(mcl, Notify())
+		.Times(1);
+
+	mcl.CountDown();
+}
+
+TEST(CountLatch, NotifyWhenCountUp)
+{
+	MockCountLatch mcl;
+
+	EXPECT_CALL(mcl, Wait(_))
+		.Times(0);
+	EXPECT_CALL(mcl, Notify())
+		.Times(0);
+
+	mcl.CountDown();
+
+	EXPECT_CALL(mcl, Notify())
+		.Times(1);
+
+	mcl.CountUp();
+}
+
+TEST(CountLatch, AwaitWhenCountIs0)
+{
+	MockCountLatch mcl;
+
+	EXPECT_CALL(mcl, Wait(_))
+		.Times(0);
+	EXPECT_CALL(mcl, Notify())
+		.Times(0);
+
+	EXPECT_EQ(0, mcl.GetCount());
+
+	mcl.Await();
+}
+
+TEST(CountLatch, AwaitWhenCountIsNot0)
+{
+	MockCountLatch mcl;
+
+	EXPECT_CALL(mcl, Wait(_))
+		.Times(0);
+	EXPECT_CALL(mcl, Notify())
+		.Times(0);
+
+	mcl.CountUp();
+
+	EXPECT_CALL(mcl, Wait(_))
+		.Times(1)
+		.WillOnce(DoAll(IgnoreResult(InvokeWithoutArgs(&mcl, &MockCountLatch::CountDown)), Return(true)));
+	EXPECT_CALL(mcl, Notify())
+		.Times(1);
+
+	EXPECT_NE(0, mcl.GetCount());
+
+	mcl.Await();
+}
+
+TEST(CountLatch, AwaitWithWaitFailure)
+{
+	MockCountLatch mcl;
+
+	EXPECT_CALL(mcl, Wait(_))
+		.Times(0);
+	EXPECT_CALL(mcl, Notify())
+		.Times(0);
+
+	mcl.CountUp();
+
+	EXPECT_CALL(mcl, Wait(_))
+		.Times(1)
+		.WillOnce(Return(false));
+	EXPECT_CALL(mcl, Notify())
+		.Times(0);
+
+	EXPECT_NE(0, mcl.GetCount());
+
+	mcl.Await();
+}
