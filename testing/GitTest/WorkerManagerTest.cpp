b7a0f17675159022ac34b71859f31bf994e375f8
murank <murank42@gmail.com>
2011-12-18 13:37:44 +0900

    Add test for WorkerManager. #10

--- testing/GitTest/WorkerManagerTest.cpp
+++ testing/GitTest/WorkerManagerTest.cpp
@@ -20,3 +20,218 @@
 #include "stdafx.h"
 #include <gmock/gmock.h>
 #include "TestHelper.h"
+
+#include "WorkerManager.h"
+
+#include "WorkerThread.h"
+
+using namespace ::testing;
+
+namespace {
+
+class MockWorkerManager : public WorkerManager {
+public:
+
+	MOCK_CONST_METHOD0(CreateWorker, shared_ptr<WorkerThread>());
+
+	using WorkerManager::RegisterActiveWorker;
+	using WorkerManager::RegisterInactiveWorker;
+};
+
+class MockWorkerThread : public WorkerThread {
+public:
+
+	MockWorkerThread() : WorkerThread(NULL) {}
+	
+	MOCK_METHOD1(AssignTask, bool(const shared_ptr<Task>& task));
+	MOCK_METHOD0(OnCancel, void());
+};
+
+} // namespace
+
+TEST(WorkerManager, Initialize)
+{
+	MockWorkerManager mwm;
+
+	EXPECT_EQ(0, mwm.GetMaxWorkers());
+
+	std::pair<size_t, size_t> num = mwm.GetNumWorkers();
+	EXPECT_EQ(0, num.first);
+	EXPECT_EQ(0, num.second);
+
+	mwm.Initialize(99);
+
+	EXPECT_EQ(99, mwm.GetMaxWorkers());
+
+	num = mwm.GetNumWorkers();
+	EXPECT_EQ(0, num.first);
+	EXPECT_EQ(0, num.second);
+}
+
+TEST(WorkerManager, RegisterTask)
+{
+	shared_ptr<MockWorkerThread> mwt(new MockWorkerThread);
+
+	EXPECT_CALL(*mwt, AssignTask(_))
+		.WillOnce(Return(true));
+
+	MockWorkerManager mwm;
+
+	EXPECT_CALL(mwm, CreateWorker())
+		.WillOnce(Return(mwt));
+
+	mwm.RegisterTask(shared_ptr<Task>());
+
+	std::pair<size_t, size_t> num = mwm.GetNumWorkers();
+	EXPECT_EQ(1, num.first);
+	EXPECT_EQ(0, num.second);
+
+	EXPECT_CALL(*mwt, OnCancel())
+		.Times(1);
+}
+
+TEST(WorkerManager, RegisterTaskToInactiveWorker)
+{
+	shared_ptr<MockWorkerThread> mwt(new MockWorkerThread);
+	MockWorkerManager mwm;
+
+	EXPECT_CALL(*mwt, AssignTask(_))
+		.WillOnce(Return(true));
+
+	EXPECT_CALL(mwm, CreateWorker())
+		.Times(0);
+
+	mwm.RegisterInactiveWorker(mwt);
+
+	std::pair<size_t, size_t> num = mwm.GetNumWorkers();
+	EXPECT_EQ(0, num.first);
+	EXPECT_EQ(1, num.second);
+
+	mwm.RegisterTask(shared_ptr<Task>());
+
+	num = mwm.GetNumWorkers();
+	EXPECT_EQ(1, num.first);
+	EXPECT_EQ(0, num.second);
+
+	EXPECT_CALL(*mwt, OnCancel())
+		.Times(1);
+}
+
+TEST(WorkerManager, RegisterTaskWithAssignTaskFailure)
+{
+	MockWorkerManager mwm;
+	shared_ptr<MockWorkerThread> mwt(new MockWorkerThread);
+
+	EXPECT_CALL(*mwt, AssignTask(_))
+		.WillOnce(Return(false))
+		.WillOnce(Return(true));
+
+	EXPECT_CALL(mwm, CreateWorker())
+		.Times(2)
+		.WillRepeatedly(Return(mwt));
+
+	mwm.RegisterTask(shared_ptr<Task>());
+
+	std::pair<size_t, size_t> num = mwm.GetNumWorkers();
+	EXPECT_EQ(1, num.first);
+	EXPECT_EQ(0, num.second);
+
+	EXPECT_CALL(*mwt, OnCancel())
+		.Times(1);
+}
+
+TEST(WorkerManager, RegisterTaskWithAssignTaskFailureAndCancellation)
+{
+	MockWorkerManager mwm;
+	shared_ptr<MockWorkerThread> mwt(new MockWorkerThread);
+
+	EXPECT_CALL(*mwt, AssignTask(_))
+		.WillOnce(DoAll(IgnoreResult(InvokeWithoutArgs(&mwm, &MockWorkerManager::CancelAsync)), Return(false)));
+
+	EXPECT_CALL(mwm, CreateWorker())
+		.WillOnce(Return(mwt));
+
+	mwm.RegisterTask(shared_ptr<Task>());
+
+	std::pair<size_t, size_t> num = mwm.GetNumWorkers();
+	EXPECT_EQ(0, num.first);
+	EXPECT_EQ(0, num.second);
+}
+
+TEST(WorkerManager, RegisterTaskAfterCancellation)
+{
+	MockWorkerManager mwm;
+
+	mwm.CancelAsync();
+
+	mwm.RegisterTask(shared_ptr<Task>());
+
+	std::pair<size_t, size_t> num = mwm.GetNumWorkers();
+	EXPECT_EQ(0, num.first);
+	EXPECT_EQ(0, num.second);
+}
+
+TEST(WorkerManager, Cancellation)
+{
+	MockWorkerManager mwm;
+	shared_ptr<MockWorkerThread> mwt1(new MockWorkerThread);
+	shared_ptr<MockWorkerThread> mwt2(new MockWorkerThread);
+
+	EXPECT_CALL(*mwt1, OnCancel())
+		.Times(1);
+	EXPECT_CALL(*mwt2, OnCancel())
+		.Times(1);
+
+	mwm.RegisterActiveWorker(mwt1);
+	mwm.RegisterInactiveWorker(mwt2);
+
+	mwm.CancelAsync();
+}
+
+TEST(WorkerManager, OnFinishTask)
+{
+	MockWorkerManager mwm;
+	shared_ptr<MockWorkerThread> mwt1(new MockWorkerThread);
+	shared_ptr<MockWorkerThread> mwt2(new MockWorkerThread);
+	shared_ptr<MockWorkerThread> mwt3(new MockWorkerThread);
+
+	mwm.RegisterActiveWorker(mwt1);
+	mwm.RegisterActiveWorker(mwt2);
+	mwm.RegisterActiveWorker(mwt3);
+
+	std::pair<size_t, size_t> num = mwm.GetNumWorkers();
+	EXPECT_EQ(3, num.first);
+	EXPECT_EQ(0, num.second);
+	EXPECT_EQ(2, mwt1.use_count());
+	EXPECT_EQ(2, mwt2.use_count());
+	EXPECT_EQ(2, mwt3.use_count());
+
+	mwm.Initialize(1);
+
+	mwm.OnFinishTask(mwt2.get());
+	num = mwm.GetNumWorkers();
+	EXPECT_EQ(2, num.first);
+	EXPECT_EQ(0, num.second);
+	EXPECT_EQ(2, mwt1.use_count());
+	EXPECT_EQ(1, mwt2.use_count());
+	EXPECT_EQ(2, mwt3.use_count());
+
+	mwm.OnFinishTask(mwt1.get());
+	num = mwm.GetNumWorkers();
+	EXPECT_EQ(1, num.first);
+	EXPECT_EQ(0, num.second);
+	EXPECT_EQ(1, mwt1.use_count());
+	EXPECT_EQ(1, mwt2.use_count());
+	EXPECT_EQ(2, mwt3.use_count());
+
+	mwm.OnFinishTask(mwt3.get());
+	num = mwm.GetNumWorkers();
+	EXPECT_EQ(0, num.first);
+	EXPECT_EQ(1, num.second);
+	EXPECT_EQ(1, mwt1.use_count());
+	EXPECT_EQ(1, mwt2.use_count());
+	EXPECT_EQ(2, mwt3.use_count());
+
+	EXPECT_CALL(*mwt3, OnCancel())
+		.Times(1);
+}
