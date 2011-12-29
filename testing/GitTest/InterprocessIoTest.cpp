5afcd4fb4a99bfc0ed8d676d6d1ad7ce9080d3a2
murank <murank42@gmail.com>
2011-12-29 13:22:10 +0900

    Add test for InterprocessIo. #10

--- testing/GitTest/InterprocessIoTest.cpp
+++ testing/GitTest/InterprocessIoTest.cpp
@@ -20,3 +20,296 @@
 #include "stdafx.h"
 #include <gmock/gmock.h>
 #include "TestHelper.h"
+
+#include "InterprocessIo.h"
+
+#include "PipeError.h"
+
+namespace {
+
+class MockInterprocessIo : public InterprocessIo {
+public:
+
+	MockInterprocessIo() : InterprocessIo(shared_handle()) {}
+
+	MOCK_METHOD3(ReadPipe, BOOL(char* buf, DWORD bytesToRead, LPDWORD bytesRead));
+	MOCK_METHOD3(WritePipe, BOOL(const char* buf, DWORD bytesToWrite, LPDWORD bytesWritten));
+
+};
+
+} // namespace
+
+using namespace ::testing;
+
+TEST(InterprocessIo, ReadInt)
+{
+	MockInterprocessIo mii;
+
+	int value = 99;
+	char* p = reinterpret_cast<char*>(&value);
+
+	EXPECT_CALL(mii, ReadPipe(NotNull(), sizeof(int), NotNull()))
+		.WillOnce(DoAll(SetArrayArgument<0>(p, p+sizeof(int)), SetArgPointee<2>(sizeof(int)), Return(TRUE)));
+
+	EXPECT_EQ(value, mii.Read<int>());
+}
+
+TEST(InterprocessIo, ReadIntWithFailureThatReturnsFalse)
+{
+	MockInterprocessIo mii;
+
+	EXPECT_CALL(mii, ReadPipe(NotNull(), sizeof(int), NotNull()))
+		.WillOnce(Return(FALSE));
+
+	EXPECT_THROW(mii.Read<int>(), pipe_error);
+}
+
+TEST(InterprocessIo, ReadIntWithFailureThatRead0Bytes)
+{
+	MockInterprocessIo mii;
+
+	EXPECT_CALL(mii, ReadPipe(NotNull(), sizeof(int), NotNull()))
+		.WillOnce(DoAll(SetArgPointee<2>(0), Return(TRUE)));
+
+	EXPECT_THROW(mii.Read<int>(), pipe_error);
+}
+
+TEST(InterprocessIo, PartialRead)
+{
+	MockInterprocessIo mii;
+
+	int value = 99;
+	char* p = reinterpret_cast<char*>(&value);
+
+	Sequence s;
+
+	for(int i=0; i<sizeof(int); ++i) {
+		EXPECT_CALL(mii, ReadPipe(NotNull(), sizeof(int)-i, NotNull()))
+			.InSequence(s)
+			.WillOnce(DoAll(SetArrayArgument<0>(p+i, p+i+1), SetArgPointee<2>(1), Return(TRUE)));
+	}
+
+	EXPECT_EQ(value, mii.Read<int>());
+}
+
+TEST(InterprocessIo, PartialReadWithFailure)
+{
+	MockInterprocessIo mii;
+
+	int value = 99;
+	char* p = reinterpret_cast<char*>(&value);
+
+	Sequence s;
+
+	for(int i=0; i<sizeof(int)/2; ++i) {
+		EXPECT_CALL(mii, ReadPipe(NotNull(), sizeof(int)-i, NotNull()))
+			.InSequence(s)
+			.WillOnce(DoAll(SetArrayArgument<0>(p+i, p+i+1), SetArgPointee<2>(1), Return(TRUE)));
+	}
+
+	EXPECT_CALL(mii, ReadPipe(NotNull(), _, NotNull()))
+		.InSequence(s)
+		.WillOnce(Return(FALSE));
+
+	EXPECT_THROW(mii.Read<int>(), pipe_error);
+}
+
+TEST(InterprocessIo, ReadCString)
+{
+	MockInterprocessIo mii;
+
+	CString value(_T("some string"));
+	int len = value.GetLength();
+	char* p1 = reinterpret_cast<char*>(&len);
+	const char* p2 = reinterpret_cast<const char*>((LPCTSTR)value);
+
+	EXPECT_CALL(mii, ReadPipe(NotNull(), sizeof(int), NotNull()))
+		.WillOnce(DoAll(SetArrayArgument<0>(p1, p1+sizeof(int)), SetArgPointee<2>(sizeof(int)), Return(TRUE)));
+
+	int clen = len*sizeof(TCHAR);
+	EXPECT_CALL(mii, ReadPipe(NotNull(), clen, NotNull()))
+		.WillOnce(DoAll(SetArrayArgument<0>(p2, p2+clen), SetArgPointee<2>(clen), Return(TRUE)));
+
+	EXPECT_EQ(value, mii.Read<CString>());
+}
+
+namespace {
+
+class InterprocessIo_P : public ::testing::TestWithParam<int> {
+};
+
+} // namepsace
+
+TEST_P(InterprocessIo_P, ReadCStringWithInvalidLength)
+{
+	MockInterprocessIo mii;
+
+	CString value(_T("some string"));
+	int invalid_len = GetParam();
+	char* p1 = reinterpret_cast<char*>(&invalid_len);
+
+	EXPECT_CALL(mii, ReadPipe(NotNull(), sizeof(int), NotNull()))
+		.WillOnce(DoAll(SetArrayArgument<0>(p1, p1+sizeof(int)), SetArgPointee<2>(sizeof(int)), Return(TRUE)));
+
+	EXPECT_EQ(CString(), mii.Read<CString>());
+}
+
+INSTANTIATE_TEST_CASE_P(Inst,
+                        InterprocessIo_P,
+                        Values(0, -1, -2));
+
+TEST(InterprocessIo, ReadCStringWithFailureOnReadingStringLength)
+{
+	MockInterprocessIo mii;
+
+	CString value(_T("some string"));
+	int len = value.GetLength();
+	char* p1 = reinterpret_cast<char*>(&len);
+	const char* p2 = reinterpret_cast<const char*>((LPCTSTR)value);
+
+	EXPECT_CALL(mii, ReadPipe(NotNull(), sizeof(int), NotNull()))
+		.WillOnce(Return(FALSE));
+
+	EXPECT_THROW(mii.Read<CString>(), pipe_error);
+}
+
+TEST(InterprocessIo, ReadCStringWithFailureOnReadingStringBytes)
+{
+	MockInterprocessIo mii;
+
+	CString value(_T("some string"));
+	int len = value.GetLength();
+	char* p1 = reinterpret_cast<char*>(&len);
+
+	EXPECT_CALL(mii, ReadPipe(NotNull(), sizeof(int), NotNull()))
+		.WillOnce(DoAll(SetArrayArgument<0>(p1, p1+sizeof(int)), SetArgPointee<2>(sizeof(int)), Return(TRUE)));
+
+	int clen = len*sizeof(TCHAR);
+	EXPECT_CALL(mii, ReadPipe(NotNull(), clen, NotNull()))
+		.WillOnce(Return(FALSE));
+
+	EXPECT_THROW(mii.Read<CString>(), pipe_error);
+}
+
+TEST(InterprocessIo, WriteInt)
+{
+	MockInterprocessIo mii;
+
+	int value = 0x11223344;
+	char* p = reinterpret_cast<char*>(&value);
+
+	EXPECT_CALL(mii, WritePipe(NotNull(), sizeof(int), NotNull()))
+		.With(Args<0, 1>(ElementsAreArray(p, sizeof(int))))
+		.WillOnce(DoAll(SetArgPointee<2>(sizeof(int)), Return(TRUE)));
+
+	EXPECT_NO_THROW(mii.Write(value));
+}
+
+TEST(InterprocessIo, WriteIntWithFailureThatReturnsFalse)
+{
+	MockInterprocessIo mii;
+
+	int value = 0x11223344;
+	char* p = reinterpret_cast<char*>(&value);
+
+	EXPECT_CALL(mii, WritePipe(NotNull(), sizeof(int), NotNull()))
+		.With(Args<0, 1>(ElementsAreArray(p, sizeof(int))))
+		.WillOnce(Return(FALSE));
+
+	EXPECT_THROW(mii.Write(value), pipe_error);
+}
+
+TEST(InterprocessIo, PartialWriteInt)
+{
+	MockInterprocessIo mii;
+
+	int value = 0x11223344;
+	char* p = reinterpret_cast<char*>(&value);
+
+	Sequence s;
+
+	for(int i=0; i<sizeof(int); ++i) {
+		EXPECT_CALL(mii, WritePipe(Pointee(*(p+i)), sizeof(int)-i, NotNull()))
+			.InSequence(s)
+			.WillOnce(DoAll(SetArgPointee<2>(1), Return(TRUE)));
+	}
+
+	EXPECT_NO_THROW(mii.Write(value));
+}
+
+TEST(InterprocessIo, PartialWriteIntWithFailure)
+{
+	MockInterprocessIo mii;
+
+	int value = 0x11223344;
+	char* p = reinterpret_cast<char*>(&value);
+
+	Sequence s;
+
+	for(int i=0; i<sizeof(int)/2; ++i) {
+		EXPECT_CALL(mii, WritePipe(Pointee(*(p+i)), sizeof(int)-i, NotNull()))
+			.InSequence(s)
+			.WillOnce(DoAll(SetArgPointee<2>(1), Return(TRUE)));
+	}
+
+	EXPECT_CALL(mii, WritePipe(Pointee(*(p+sizeof(int)/2)), sizeof(int)-sizeof(int)/2, NotNull()))
+		.InSequence(s)
+		.WillOnce(Return(FALSE));
+
+	EXPECT_THROW(mii.Write(value), pipe_error);
+}
+
+TEST(InterprocessIo, WriteCString)
+{
+	MockInterprocessIo mii;
+
+	CString value("some string");
+	int len = value.GetLength();
+	char* p = const_cast<char*>(reinterpret_cast<const char*>((LPCTSTR)value));
+
+	InSequence s;
+
+	EXPECT_CALL(mii, WritePipe(NotNull(), sizeof(int), NotNull()))
+		.With(Args<0, 1>(ElementsAreArray(reinterpret_cast<char*>(&len), sizeof(int))))
+		.WillOnce(DoAll(SetArgPointee<2>(sizeof(int)), Return(TRUE)));
+	EXPECT_CALL(mii, WritePipe(NotNull(), len*sizeof(TCHAR), NotNull()))
+		.With(Args<0, 1>(ElementsAreArray(p, len*sizeof(TCHAR))))
+		.WillOnce(DoAll(SetArgPointee<2>(len*sizeof(TCHAR)), Return(TRUE)));
+
+	EXPECT_NO_THROW(mii.Write(value));
+}
+
+TEST(InterprocessIo, WriteCStringWithFailureOnWritingStringLength)
+{
+	MockInterprocessIo mii;
+
+	CString value("some string");
+	int len = value.GetLength();
+	char* p = const_cast<char*>(reinterpret_cast<const char*>((LPCTSTR)value));
+
+	EXPECT_CALL(mii, WritePipe(NotNull(), sizeof(int), NotNull()))
+		.With(Args<0, 1>(ElementsAreArray(reinterpret_cast<char*>(&len), sizeof(int))))
+		.WillOnce(Return(FALSE));
+
+	EXPECT_THROW(mii.Write(value), pipe_error);
+}
+
+TEST(InterprocessIo, WriteCStringWithFailureOnWritingStringBytes)
+{
+	MockInterprocessIo mii;
+
+	CString value("some string");
+	int len = value.GetLength();
+	char* p = const_cast<char*>(reinterpret_cast<const char*>((LPCTSTR)value));
+
+	InSequence s;
+
+	EXPECT_CALL(mii, WritePipe(NotNull(), sizeof(int), NotNull()))
+		.With(Args<0, 1>(ElementsAreArray(reinterpret_cast<char*>(&len), sizeof(int))))
+		.WillOnce(DoAll(SetArgPointee<2>(sizeof(int)), Return(TRUE)));
+	EXPECT_CALL(mii, WritePipe(NotNull(), len*sizeof(TCHAR), NotNull()))
+		.With(Args<0, 1>(ElementsAreArray(p, len*sizeof(TCHAR))))
+		.WillOnce(Return(FALSE));
+
+	EXPECT_THROW(mii.Write(value), pipe_error);
+}
