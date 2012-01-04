d4edd53238baf50ae72107c4afae77a60c273fe0
murank <murank42@gmail.com>
2012-01-04 14:36:17 +0900

    Create TypeTraits. #10

---
+++ src/Git/TypeTraits.h
@@ -0,0 +1,28 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// External Cache Copyright (C) 2005 - 2006 - Will Dean, Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#ifndef TYPE_TRAITS_H
+#define TYPE_TRAITS_H
+
+#include <type_traits>
+
+using std::tr1::remove_reference;
+using std::tr1::remove_cv;
+
+#endif
