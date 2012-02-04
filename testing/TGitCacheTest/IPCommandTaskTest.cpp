ac175d69d34e02a2f426eb7156a649aac2b7559f
murank <murank42@gmail.com>
2012-02-04 21:18:32 +0900

    Use precompiled headers in test projects. #17

--- testing/TGitCacheTest/IPCommandTaskTest.cpp
+++ testing/TGitCacheTest/IPCommandTaskTest.cpp
@@ -17,9 +17,7 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 
-#include "stdafx.h"
-#include <gmock/gmock.h>
-#include "TestHelper.h"
+#include "StdAfxTest.h"
 
 #include "IPCommandTask.h"
 
